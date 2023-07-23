c4a7408ccc6f045975bfcd95e83ff029c634473e
Richard Lee <richardleecy123@gmail.com>
2023-07-23 12:22:33 +0800

    update model relationship

--- Triphub/app/Models/Idea.php
+++ Triphub/app/Models/Idea.php
@@ -9,17 +9,22 @@ class Idea extends Model
 {
     use HasFactory;
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
     public function images()
     {
         return $this->hasMany(IdeasImage::class);
     }
 
-    public function Comments()
+    public function comments()
     {
         return $this->hasMany(IdeasComment::class);
     }
 
-    public function Tags()
+    public function tags()
     {
         return $this->hasMany(IdeasTag::class);
     }
